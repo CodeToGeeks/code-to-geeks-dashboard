fc379f63e33d41c1ae3c359c4ea49a25fdc0b790
Ahmed Ali Ahmed <47031477+AhmedAliAbdAlMowla@users.noreply.github.com>
2022-06-25 13:36:35 +0200

    Update configAxiosDefultValues.js

--- src/config/configAxiosDefultValues.js
+++ src/config/configAxiosDefultValues.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const configAxios = () => {
-  axios.defaults.baseURL = "http://localhost:4000/api/v1";
+  axios.defaults.baseURL = process.env.API_URL || "http://localhost:4000/api/v1";
   
   // "https://codetogeeksapi.herokuapp.com/api/v1";
   //
